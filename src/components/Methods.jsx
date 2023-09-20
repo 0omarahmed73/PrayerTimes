a298a7572aa1a0b438d7cd533d06aeec3016bc7e
Omar Ahmed Saeed <85828826+0omarahmed73@users.noreply.github.com>
2023-09-20 14:43:06 +0300

    Update Methods.jsx

--- src/components/Methods.jsx
+++ src/components/Methods.jsx
@@ -1,13 +1,13 @@
 const Methods = () => {
   return (
     <>
-      <option value="1">Muslim World League</option>
+      <option value="1">University of Islamic Sciences, Karachi</option>
       <option value="2">Islamic Society of North America</option>
-      <option value="3">Egyptian General Authority of Survey</option>
-      <option value="4">Umm Al-Qura University, Makkah</option>
-      <option value="5">University of Islamic Sciences, Karachi</option>
-      <option value="6">Institute of Geophysics, University of Tehran</option>
-      <option value="7">Shia Ithna-Ashari, Leva Institute, Qum</option>
+      <option value="3">Muslim World League</option>
+      <option value="4">Islamic Society of North America</option>
+      <option value="5">Egyptian General Authority of Survey</option>
+      <option value="6">Umm Al-Qura University, Makkah</option>
+      <option value="7">Institute of Geophysics, University of Tehran</option>
       <option value="8">Gulf Region</option>
       <option value="9">Kuwait</option>
       <option value="10">Qatar</option>
